bd11217d6a3c783e30257bac6027c1e0b97b0f0f
Gusev Roman <114294990+GhenntoggyUTM@users.noreply.github.com>
2023-05-19 18:18:09 +0300

    Add Header
    added a search bar, buttons for such pages as: Help, Sign in / Sign up; and actions: Search;
    added links to Presidency website
    added logos for Presidency

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,50 @@
 import React from 'react'
-import { Col, Row } from 'antd';
-import { Button, Space } from 'antd';
-import { Anchor } from 'antd';
-
-const { Link } = Anchor;
+import { Link, Text, Button, Box, Flex, Grid, Input, IconButton, InputRightElement, InputGroup, Image, Stack} from '@chakra-ui/react'
+import { SearchIcon } from '@chakra-ui/icons'
 
 export const Header = () => {
   return (
     <>
-    <Row>
-      <Col span={12}>
-        <Space wrap>
-          <Anchor>
-            <Link href="https://presedinte.md/" title="Site-ul oficial al Preşedinţiei">
+    <Grid templateColumns="repeat(16, 1fr)" gap={4}>
+      <Box gridColumn="span 2" />
+      <Box gridColumn="span 12" sx={{ display: 'center' }} justifyContent='center' borderBottomWidth='1px' borderBottomLeftRadius='lg' borderRightWidth='1px' borderBottomRightRadius='lg' borderLeftWidth='1px' width="auto" padding='1.5'>
+          <Text fontSize='sm'>
+            <Link href='https://presedinte.md/' >
+              <Stack direction='row'>
+                <Image src="https://upload.wikimedia.org/wikipedia/commons/4/4f/Emblema_Guvernului_Republicii_Moldova.png" boxSize='20px'/> 
+                <Text fontSize='sm'>Site-ul oficial al Preşedinţiei RM</Text>
+              </Stack>
             </Link>
-          </Anchor>
-        </Space></Col>
-    </Row>
-    <Row>
-      <Col span={12}>col-12</Col>
-      <Col span={12}>col-12</Col>
-    </Row>
-  </>
+          </Text>
+      <Flex marginLeft="auto" alignItems="center" paddingRight="1rem">
+        <Button size='sm' as="a" href="#"  marginRight="0.5rem" variant="link" color='black' fontSize='sm' fontWeight='light'>
+          Ajutor
+        </Button>
+      <Box width="1px" height="20px" backgroundColor="gray.200" marginRight="0.5rem" />
+        <Button size='sm' as="a" href="#" variant="link" color='black' fontSize='sm' fontWeight='light'>
+          Sign in / Sign up
+        </Button>
+      </Flex>
+      </Box>
+    </Grid>
+    
+    <Flex alignItems="center" paddingLeft="12.5rem" paddingRight="12.5rem" paddingTop='1rem' paddingBottom='0.5rem' borderBottomWidth='1px' borderBottomRadius='lg' borderRightWidth='1px' borderRightRadius='lg' borderLeftWidth='1px' borderLeftRadius='lg'>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Coat_of_arms_of_Moldova.svg/640px-Coat_of_arms_of_Moldova.svg.png" alt="Site Logo" width="70px" height="70px" />
+      <Box marginLeft="1rem" fontFamily="inherit" fontSize="18px" paddingTop='1rem'>
+        <Text fontSize='2xl' as='b'>PETIŢII ELECTRONICE</Text>
+        <Text fontSize='smaller'>Reprezentanţa oficială online a Preşedenţiei RM</Text>
+        <br />
+      </Box>
+      <Button width='auto' marginLeft="auto" marginRight="auto" borderRadius='3xl' fontWeight='light' colorScheme='blue'>Creaţi o petiţie</Button>
+      <Flex alignItems="center" paddingRight="0rem">
+          <InputGroup>
+            <Input placeholder="Căutaţi petiţia" borderRadius='3xl'/>
+            <InputRightElement>
+              <IconButton colorScheme='blue' aria-label='Search database' borderRadius='3xl' icon={<SearchIcon />} />
+            </InputRightElement>
+          </InputGroup>
+        </Flex>
+    </Flex>
+    </>
   )
 }
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,5 @@
 import { Petition } from "types";
-
+import { Header } from "components";
 import { Container, Heading, Stack, Text, Button } from "@chakra-ui/react";
 
 const petition: Petition = {
@@ -44,44 +44,7 @@ const petition: Petition = {
 export const App = () => {
   return (
     <div className="App">
-      <Container maxW={"5xl"}>
-        <Stack
-          textAlign={"center"}
-          align={"center"}
-          spacing={{ base: 8, md: 10 }}
-          py={{ base: 20, md: 28 }}
-        >
-          <Heading
-            fontWeight={600}
-            fontSize={{ base: "3xl", sm: "4xl", md: "6xl" }}
-            lineHeight={"110%"}
-          >
-            Meeting scheduling{" "}
-            <Text as={"span"} color={"primary.400"}>
-              made easy
-            </Text>
-          </Heading>
-          <Text color={"gray.500"} maxW={"3xl"}>
-            Never miss a meeting. Never be late for one too. Keep track of your meetings and receive
-            smart reminders in appropriate times. Read your smart “Daily Agenda” every morning.
-          </Text>
-          <Stack spacing={6} direction={"row"}>
-            <Button
-              rounded={"full"}
-              px={6}
-              colorScheme={"primary"}
-              bg={"primary.400"}
-              _hover={{ bg: "primary.500" }}
-            >
-              Get started
-            </Button>
-            <Button rounded={"full"} px={6}>
-              Learn more
-            </Button>
-          </Stack>
-        </Stack>
-      </Container>
-      );
+      <Header></Header>
     </div>
   );
 };
