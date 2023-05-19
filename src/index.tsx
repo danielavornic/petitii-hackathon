262fea5cec3505b00e2381cd07b7c653a303b469
Daniela Vornic <daniela.vornic@gmail.com>
2023-05-19 14:29:49 +0300

    Add chakra

--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,33 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ChakraProvider, baseTheme, extendTheme } from "@chakra-ui/react";
 
 import { UserProvider } from "context";
 
-import "antd/dist/reset.css";
+const theme = extendTheme({
+  fonts: {
+    heading: `'Inter', sans-serif`,
+    body: `'Inter', sans-serif`,
+  },
+  colors: {
+    primary: baseTheme.colors.cyan,
+  },
+  withDefaultColorScheme: {
+    colorScheme: "primary",
+  },
+});
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <UserProvider>
-        <RouterProvider router={router} />
-      </UserProvider>
-    </QueryClientProvider>
+    <ChakraProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <UserProvider>
+          <RouterProvider router={router} />
+        </UserProvider>
+      </QueryClientProvider>
+    </ChakraProvider>
   </React.StrictMode>,
 );
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,13 +1,87 @@
-import { Button, DatePicker, Space, version } from "antd";
+import { Petition } from "types";
+
+import { Container, Heading, Stack, Text, Button } from "@chakra-ui/react";
+
+const petition: Petition = {
+  id: 1,
+  initiator: {
+    name: "John",
+    surname: "Doe",
+    id: "123456789",
+    location: "New York",
+    isResident: true,
+    birthDate: new Date("1990-01-01"),
+  },
+  name: "Awarding the title of Hero of Ukraine (posthumously) to Mazurok soldier Vitaly Serhiyovych.",
+  date: new Date(),
+  nrSign: 1000,
+  content: "We urge the government to invest in renewable energy sources.",
+  toWho: "Government officials",
+  statut: "Open",
+  semnat: [
+    {
+      name: "Alice",
+      surname: "Smith",
+      id: "987654321",
+      location: "California",
+      isResident: true,
+      birthDate: new Date("1985-05-10"),
+    },
+    {
+      name: "Bob",
+      surname: "Johnson",
+      id: "654321987",
+      location: "Texas",
+      isResident: true,
+      birthDate: new Date("1992-09-20"),
+    },
+  ],
+  feedback: "Thank you for your support!",
+  deadLine: new Date("2023-06-30"),
+  Category: ["Environment", "Energy"],
+};
 
 export const App = () => {
   return (
     <div className="App">
-      <h1>antd version: {version}</h1>
-      <Space>
-        <DatePicker />
-        <Button type="primary">Primary Button</Button>
-      </Space>
+      <Container maxW={"5xl"}>
+        <Stack
+          textAlign={"center"}
+          align={"center"}
+          spacing={{ base: 8, md: 10 }}
+          py={{ base: 20, md: 28 }}
+        >
+          <Heading
+            fontWeight={600}
+            fontSize={{ base: "3xl", sm: "4xl", md: "6xl" }}
+            lineHeight={"110%"}
+          >
+            Meeting scheduling{" "}
+            <Text as={"span"} color={"primary.400"}>
+              made easy
+            </Text>
+          </Heading>
+          <Text color={"gray.500"} maxW={"3xl"}>
+            Never miss a meeting. Never be late for one too. Keep track of your meetings and receive
+            smart reminders in appropriate times. Read your smart “Daily Agenda” every morning.
+          </Text>
+          <Stack spacing={6} direction={"row"}>
+            <Button
+              rounded={"full"}
+              px={6}
+              colorScheme={"primary"}
+              bg={"primary.400"}
+              _hover={{ bg: "primary.500" }}
+            >
+              Get started
+            </Button>
+            <Button rounded={"full"} px={6}>
+              Learn more
+            </Button>
+          </Stack>
+        </Stack>
+      </Container>
+      );
     </div>
   );
 };
