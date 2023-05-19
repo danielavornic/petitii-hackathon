dd90f04b3f80b0ac0dde67c9f08924d7fe38c671
Daniela Vornic <daniela.vornic@gmail.com>
2023-05-19 18:26:00 +0300

    Change routes to jsx + init petition page

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import { router } from "router";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ChakraProvider, baseTheme, extendTheme } from "@chakra-ui/react";
 
 import { UserProvider } from "context";
+import { App, Petition } from "pages";
+
 import "./styles/index.css";
 
 const theme = extendTheme({
@@ -29,7 +30,12 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <ChakraProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
         <UserProvider>
-          <RouterProvider router={router} />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<App />} />
+              <Route path="petition/:petitionId" element={<Petition />} />
+            </Routes>
+          </BrowserRouter>
         </UserProvider>
       </QueryClientProvider>
     </ChakraProvider>
---
+++ src/pages/Petition.tsx
@@ -0,0 +1,5 @@
+import { Container } from "@chakra-ui/react";
+
+export const Petition = () => {
+  return <Container maxW="8xl"></Container>;
+};
